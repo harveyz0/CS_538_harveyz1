08a61e5ac0f79dfbdd361e514590cbbb372a0b10
Zachary Harvey <harveyz1@sunypoly.edu>
2024-01-31 15:25:27 -0500

    Configured tester to use doctest
    tester.cpp is configured to use doctest. This was to test cmake
    changes.

--- src/lib/tester.cpp
+++ src/lib/tester.cpp
@@ -1,10 +1,13 @@
-
+#define DOCTEST_CONFIG_IMPLEMENT
 #include <iostream>
 #include <typeinfo>
+#include "doctest/doctest.h"
 #include "vec.hpp"
 using namespace std;
 
+
 int main(){
+//TEST_CASE("ONE"){
     Vec3<int>* vi = new Vec3<int>(1,2,3);
     cout << "vi " << vi->x << " " << vi->y << " " << vi->z << endl;
     Vec3<float> vf(1.5,2.5, 3.5);
@@ -15,6 +18,6 @@ int main(){
     // The previous line prints the following
     // va 3 5 7 4Vec3IdE
     // I have no idea what 4Vec3IdE means.
-    return 0;
+    //return 0;
 }
 
