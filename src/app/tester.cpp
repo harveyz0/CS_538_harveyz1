b121306dd4058ae3101d11340a4c2eba5ff2fcee
Zachary Harvey <harveyz1@sunypoly.edu>
2024-02-22 16:23:20 -0500

    Merge in a stash I had from a while ago

--- src/app/tester.cpp
+++ src/app/tester.cpp
@@ -10,11 +10,12 @@ int main(){
 //TEST_CASE("ONE"){
     Vec3<int>* vi = new Vec3<int>(1,2,3);
     cout << "vi " << vi->x << " " << vi->y << " " << vi->z << endl;
-    Vec3<float> vf(1.5,2.5, 3.5);
-    Vec3<double> vd(1.5,2.5, 3.5);
+    Vec3<float> vf(1.25f,2.5f, 3.5f);
+    Vec3<double> vd(1.3f,2.5f, 3.5f);
     auto va = vf + vd;
+    float p = 1.33f;
 
-    cout << "va " << va.x << " " << va.y << " " << va.z << " " << typeid(va).name() << endl;
+    cout << "va " << va.x << " " << va.y << " " << va.z << " " << typeid(p).name() << endl;
     // The previous line prints the following
     // va 3 5 7 4Vec3IdE
     // I have no idea what 4Vec3IdE means.
