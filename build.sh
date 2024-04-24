7228af5042118c8b0aa256677b8aed572313c6cf
Zachary Harvey <harveyz1@sunypoly.edu>
2024-04-24 16:38:26 -0400

    Fix build.sh to use PWD instead of hardcoded directory

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-PROJ_DIR="/home/zac/Documents/classes/CS538/CS_538_harveyz1"
+PROJ_DIR="$PWD"
 rm -rf $PROJ_DIR/build
 cmake -DCMAKE_BUILD_TYPE:STRING=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=YES -DCMAKE_C_COMPILER=/usr/bin/clang -DCMAKE_CXX_COMPILER=/usr/bin/clang++ -S$PROJ_DIR -B$PROJ_DIR/build -G Unix\ Makefiles
 cd $PROJ_DIR/build
